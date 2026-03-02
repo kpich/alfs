0515bc1b642c465ba0f6775c552c78294b69fc92
Karl Pichotta <karlpichotta@gmail.com>
2026-03-01 23:16:27 -0500

    fix mypy errors (#73)

--- src/alfs/update/induction/induce_senses.py
+++ src/alfs/update/induction/induce_senses.py
@@ -95,7 +95,7 @@ def main() -> None:
     docs_df = (
         pl.scan_parquet(args.docs)
         .filter(pl.col("doc_id").is_in(needed_doc_ids))
-        .collect(streaming=True)
+        .collect(engine="streaming")
     )
     docs = dict(
         zip(docs_df["doc_id"].to_list(), docs_df["text"].to_list(), strict=False)
